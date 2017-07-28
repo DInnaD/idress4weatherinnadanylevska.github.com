94e472f03e992697dff4844106e8a8c980237bb4
idress4weatherinnadanylevska <inna.danylevska@gmail.com>
2017-07-28 11:43:02 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,37 @@
 $(function() {
+	var ball = document.getElementById('ball');
+
+ball.onmousedown = function(e) {
+
+  var coords = getCoords(ball);
+  var shiftX = e.pageX - coords.left;
+  var shiftY = e.pageY - coords.top;
+
+  ball.style.position = 'absolute';
+  document.body.appendChild(ball);
+  moveAt(e);
+
+  ball.style.zIndex = 1000; // над другими элементами
+
+  function moveAt(e) {
+    ball.style.left = e.pageX - shiftX + 'px';
+    ball.style.top = e.pageY - shiftY + 'px';
+  }
+
+  document.onmousemove = function(e) {
+    moveAt(e);
+  };
+
+  ball.onmouseup = function() {
+    document.onmousemove = null;
+    ball.onmouseup = null;
+  };
+
+}
+
+ball.ondragstart = function() {
+  return false;
+};
   // code for loader
   $(document).ajaxStart(function() {
     //show loader animation and hide border
